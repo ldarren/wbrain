24a81bfa9afdcecef787e4e74a128764cb30cfae
Darren Liew <ldarren@Akamaru.local>
2015-04-22 10:56:18 +0800

    refactoring

--- think.js
+++ think.js
@@ -2,10 +2,11 @@ var
 fs = require('fs'),
 NeuralNetwork = require('./NeuralNetwork'),
 DataIris = require('./DataIris'),
+set = process.argv[2],
 inputs = [],
 targets = []
 
 DataIris.read('data/bezdekIris.data', inputs, targets)
 
 var nn = new NeuralNetwork(JSON.parse(fs.readFileSync('./memory',{encoding:'utf8'})))
-console.log(targets[0], nn.think(inputs[0]))
+console.log(targets[set], nn.think(inputs[set]))
